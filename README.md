9c7acfc38ea339f0b0f706d0b602770a85c81cd4
bdkruse <bkruse2@madisoncollege.edu>
2018-06-20 11:29:08 -0500

    add database dump, update readme

--- README.md
+++ README.md
@@ -3,17 +3,25 @@ This is the PHP API component that is a requirement for the [front to back data 
 
 ### Prerequisites
 ```
-A web server/stack that can handle MySQL and PHP
+Web Server Stack supporting MySQL and PHP, something like:
+LAMP https://bitnami.com/stack/lamp/installer
+WAMP http://www.wampserver.com/en/
 ```
 
 ### Getting Started
 Download or clone this repository into your web hosting folder, the folder should be called "fronttoback-mysql-php-api"
 
-Create a database using the datadump.sql file in the root directory
+Create a database named mysql_php_demo by following the commandline directions or use your favorite application.
+
+After logging into MySQL
 ```
-placeholder
+mysql> CREATE DATABASE mysql_php_demo;
 ```
-Configure the connection using the src/config/db.php file if necessary.
+Open command line and navigate to your mysql installiation bin folder. Run the following, adjusting the file path to the mysql-php-dump.sql file in the root directory of this project.
+```
+mysql -u [username] -p mysql_php_demo < _path_/_to_/_file_/mysql-php-dump.sql
+```
+Configure the connection using the src/config/db.php file to match your mysql login credientials. 
 
 Start your web server and test the api is working using: 
 ```
---
+++ mysql-php-dump.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mysql_php_demo
+-- ------------------------------------------------------
+-- Server version	5.7.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `roster`
+--
+
+DROP TABLE IF EXISTS `roster`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roster` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `playername` varchar(20) DEFAULT NULL,
+  `team` varchar(30) DEFAULT NULL,
+  `jerseynumber` varchar(10) DEFAULT NULL,
+  `position` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=66 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roster`
+--
+
+LOCK TABLES `roster` WRITE;
+/*!40000 ALTER TABLE `roster` DISABLE KEYS */;
+INSERT INTO `roster` VALUES (1,'Randall Cobb','Green Bay Packers','18','Wide Receiver'),(49,'Jerry Rice','San Francisco 49ers','80','Wide Receiver');
+/*!40000 ALTER TABLE `roster` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'mysql_php_demo'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-20 10:29:12
