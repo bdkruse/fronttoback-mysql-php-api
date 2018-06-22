73598147c9df034c70878f6e72f5720426b0f1ab
bdkruse <bkruse2@madisoncollege.edu>
2018-06-22 13:18:07 -0500

    typo in db.php

--- src/config/db.php
+++ src/config/db.php
@@ -9,7 +9,7 @@
         //connect
         public function connect(){
             $mysql_connect_str= "mysql:host=$this->dbhost;dbname=$this->dbname";
-            $dbConnection = new PDO($mysql_connect_str, $this->dbuser. $this->dbpass);
+            $dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass);
             $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $dbConnection;
         }
